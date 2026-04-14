1899102fdba55bd852ea0ecaa934c1057a7f1efa
Ryan <ryanak@gmail.com>
2026-04-14 01:24:38 -0700

    update logic, begin different words

--- src/components/Wordle.tsx
+++ src/components/Wordle.tsx
@@ -1,24 +1,41 @@
-import { useState, useRef } from "react";
+import { useState, useRef, useEffect } from "react";
 import styles from "./Wordle.module.css";
 import { setup } from "../data/setup";
+import { words } from "../data/words";
+
+const word = words[Math.floor(Math.random() * (words.length - 1 + 1)) + 1];
 
 export function Wordle() {
   const input = useRef<HTMLInputElement | null>(null);
-  const secret = "ocean";
+  const secret = word;
   const [tries, setTries] = useState(0);
-  const [success, setSuccess] = useState(false);
+  const [success, setSuccess] = useState<null | Boolean>(null);
   const [guesses, setGuesses] = useState(setup);
 
-  function handleGuess() {
 
-    if (tries >= 5 || success) return;
-    
+  function handleReset(e: React.MouseEvent<HTMLButtonElement>) {
+    e.preventDefault();
+    setSuccess(null);
+    setGuesses(setup);
+    setTries(0);
+  }
+
+  function handleMissed() {
+    setSuccess(false);
+    setGuesses(setup);
+    setTries(0);
+  }
+
+  useEffect(() => {
+    if (tries === 5 && !success) handleMissed();
+  }, [tries])
+
+  function handleGuess() {
     const guess = input.current?.value.toString();
-    if (guess === secret) setSuccess(true);
-    
+    if (!guess) return;
+    if (tries >= 5 || success ) return;
     const newTry = tries + 1;
     setTries(newTry);
-
     const secretArray = secret?.split("");
     const guessArray = guess?.split("");
     let correctGuesses: string[] = [];
@@ -38,12 +55,14 @@ export function Wordle() {
     setGuesses((previousState) => {
       return { ...previousState, [newTry]: orderedGuess };
     });
+
+    if (guess === secret) setSuccess(true);
   }
 
   return (
     <div>
       <h1>Wordle</h1>
-      {success && <h2>You Won! It was "{secret}"</h2>}
+      <h2>{success === true ? `You Won! It was "${secret}"` : success === false ? 'Sorry! Try again!' : ''}</h2>
       <div className={styles.wordle}>
         {Object.values(guesses).map((guess) => {
           return (
@@ -61,15 +80,23 @@ export function Wordle() {
         })}
       </div>
       <form action={handleGuess}>
+
         <input
           className={styles.guessInput}
           ref={input}
           type="text"
           maxLength={5}
+          disabled={success === false ? true : false}
         />
-        <button className={styles.guessSubmit} type="submit">
-          Submit
-        </button>
+
+
+        {(success == true || success == false) ? (
+          <button className={styles.guessReset} type="button" onClick={(e) => handleReset(e) }>Reset</button>
+        ) : (
+          <button className={styles.guessSubmit} type="submit">
+            Submit
+          </button>
+        )}
       </form>
     </div>
   );
